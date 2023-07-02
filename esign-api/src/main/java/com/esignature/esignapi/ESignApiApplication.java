4fd67168594dddedc1961fd647de5b0865053c34
Aamol Gote <Amol.Gote@icreditworks.com>
2023-07-02 16:00:35 -0400

    First Sping draft template

---
+++ esign-api/src/main/java/com/esignature/esignapi/ESignApiApplication.java
@@ -0,0 +1,11 @@
+package com.esignature.esignapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESignApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ESignApiApplication.class, args);
+	}
+}
